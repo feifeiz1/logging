541bd4b000dfbafafbec356b70e58d678b358728
yifei <yifei@micos.com>
2025-03-05 14:27:31 +0800

    update

---
+++ logging.go
@@ -0,0 +1,33 @@
+package logging
+
+import "go.uber.org/zap"
+
+var logger *zap.SugaredLogger
+
+func Init() {
+	logger = zap.NewExample().Sugar().WithOptions()
+}
+
+func Info(args ...interface{}) {
+	logger.Info(args)
+}
+
+func Infof(template string, args ...interface{}) {
+	logger.Infof(template, args)
+}
+
+func Warn(args ...interface{}) {
+	logger.Warn(args)
+}
+
+func Warnf(template string, args ...interface{}) {
+	logger.Warnf(template, args)
+}
+
+func Error(args ...interface{}) {
+	logger.Error(args)
+}
+
+func Errorf(template string, args ...interface{}) {
+	logger.Errorf(template, args)
+}
